54d0aecb72ce136daf1954a05abb32e0002365f4
JyotsnaRajaraman <r.jyotsna.1999@gmail.com>
2026-07-24 12:39:48 -0500

    get specific

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 
-from auth import router as auth_router
-from oura import (
+from app.auth import router as auth_router
+from app.oura import (
     get_daily_sleep,
     get_daily_activity,
     get_daily_readiness,
@@ -20,8 +20,21 @@ def root():
 @app.get("/api/trmnl")
 def trmnl():
 
+    sleep = get_daily_sleep()["data"][0]
+    readiness = get_daily_readiness()["data"][0]
+    activity = get_daily_activity()["data"][0]
+
     return {
-        "sleep": get_daily_sleep(),
-        "activity": get_daily_activity(),
-        "readiness": get_daily_readiness(),
+        "date": sleep["day"],
+
+        "sleep_score": sleep["score"],
+        "sleep_duration": sleep["contributors"]["total_sleep"],
+
+        "readiness_score": readiness["score"],
+        "hrv": readiness["contributors"]["hrv_balance"],
+        "resting_hr": readiness["contributors"]["resting_heart_rate"],
+
+        "activity_score": activity["score"],
+        "steps": activity["steps"],
+        "calories": activity["active_calories"],
     }
