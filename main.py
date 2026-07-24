dc33730b2a1eef7f0ca6ddc3a9c5ad5d98d60911
JyotsnaRajaraman <r.jyotsna.1999@gmail.com>
2026-07-23 22:04:22 -0500

    main:debug login

--- main.py
+++ main.py
@@ -14,14 +14,35 @@ def home():
     return {"status": "running"}
 
 
+# @app.get("/login")
+# def login():
+#     state = secrets.token_urlsafe(16)
+
+#     scopes = [
+#         "daily",
+#         "heartrate",
+#         "personal",
+#     ]
+
+#     url = (
+#         "https://cloud.ouraring.com/oauth/authorize?"
+#         f"response_type=code"
+#         f"&client_id={CLIENT_ID}"
+#         f"&redirect_uri={REDIRECT_URI}"
+#         f"&scope={' '.join(scopes)}"
+#         f"&state={state}"
+#     )
+
+#     return RedirectResponse(url)
+
 @app.get("/login")
 def login():
     state = secrets.token_urlsafe(16)
 
     scopes = [
         "daily",
-        "heartrate",
         "personal",
+        "heartrate",
     ]
 
     url = (
@@ -33,7 +54,7 @@ def login():
         f"&state={state}"
     )
 
-    return RedirectResponse(url)
+    return {"url": url}
 
 
 @app.get("/oauth/callback")
